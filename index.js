0d2802266e581e4c5e84500e7f6bdc2027feb2cd
+stephen.nene <+>
2022-11-24 16:43:53 +0300

    completed arithmetic js assignmnet

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+let random =(Math.floor(Math.random() * 10) + 1)
+console.log(random)
+
+const num3 = 24;
+const num4 = 5;
+const mod = (num3%num4)
+console.log(num3%num4)
+
+const max = Math.max(1,7,11,19,14,20)
+
+
+// Math.floor(Math.random() * 10) + 1;
